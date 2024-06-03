9111d19cac136753d5b61ce473e455cbfac95706
fonti <ivan.fontana@xfarm.ag>
2024-06-03 11:51:42 +0200

    Add line number toggle <c-n>

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -7,7 +7,7 @@ return {
 				null_ls.builtins.formatting.stylua, -- lua
 				null_ls.builtins.formatting.black, -- python
 				null_ls.builtins.diagnostics.mypy,
-				null_ls.builtins.diagnostics.ruff,
+				-- null_ls.builtins.diagnostics.ruff,
 				null_ls.builtins.formatting.clang_format, -- c/c++
 			},
 		})
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -4,7 +4,7 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set expandtab")
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<c-n>", ":set number toggle<CR>")
+vim.keymap.set("n", "<c-n>", ":set invnumber<CR>")
 
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
