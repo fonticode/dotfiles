9ee0a014c07bfeb0fa82ce51078f31754c982b09
fonti <ivan.fontana3@gmail.com>
2024-08-19 21:42:41 +0200

    Update extras

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -39,6 +39,8 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.markdown" },
 	{ import = "lazyvim.plugins.extras.lang.python" },
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.lang.clangd" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
