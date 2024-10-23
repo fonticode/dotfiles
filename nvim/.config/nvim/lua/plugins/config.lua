05000c1df20924ba78bc6cc1bee245be07119733
fonti <ivan.fontana3@gmail.com>
2024-10-23 10:05:26 +0200

    add rust extras

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -41,6 +41,7 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
 	{ import = "lazyvim.plugins.extras.lang.clangd" },
 	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.rust" },
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
