8e1545870b8e4638d782522d5564d91950cc0736
fonti <ivan.fontana3@gmail.com>
2025-05-23 16:12:14 +0200

    update nvim config & fix warning

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -1,10 +1,50 @@
 return {
+
+	--- Theme
 	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "catppuccin",
 		},
 	},
+
+	---	Extras:
+	---		Add "vim.g.lazyvim_check_order = false" in ~/.config/nvim/lua/config/options.lua
+	---		To disblable plugin order warning (unadressed issue)
+	{ import = "lazyvim.plugins.extras.lang.cmake" },
+	{ import = "lazyvim.plugins.extras.lang.docker" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.python" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.lang.clangd" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.rust" },
+	{ import = "lazyvim.plugins.extras.lang.toml" },
+	{ import = "lazyvim.plugins.extras.dap.core" },
+
+	--- Treesitter syntax highlighting (not included in extras)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"csv",
+				"make",
+				"doxygen",
+				"rasi",
+				"tmux",
+				"usd",
+				"cuda",
+				"glsl",
+				"wgsl",
+				"wgsl_bevy",
+				"gdscript",
+				"gdshader",
+				"zathurarc",
+			},
+		},
+	},
+
+	--- Neo-tree unhide dotfiles
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		opts = {
@@ -17,6 +57,8 @@ return {
 			},
 		},
 	},
+
+	--- Vim Tmux Navigator
 	{
 		"christoomey/vim-tmux-navigator",
 		cmd = {
@@ -34,28 +76,12 @@ return {
 			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 		},
 	},
-	{ import = "lazyvim.plugins.extras.lang.cmake" },
-	{ import = "lazyvim.plugins.extras.lang.docker" },
-	{ import = "lazyvim.plugins.extras.lang.markdown" },
-	{ import = "lazyvim.plugins.extras.lang.python" },
-	{ import = "lazyvim.plugins.extras.lang.yaml" },
-	{ import = "lazyvim.plugins.extras.lang.clangd" },
-	{ import = "lazyvim.plugins.extras.lang.json" },
-	{ import = "lazyvim.plugins.extras.lang.rust" },
-	{ import = "lazyvim.plugins.extras.lang.toml" },
-	{ import = "lazyvim.plugins.extras.dap.core" },
+
+	--- Hardtime (learn proper vim motions)
 	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"cmake",
-				"cpp",
-				"csv",
-				"cuda",
-				"dockerfile",
-				"doxygen",
-				"make",
-			},
-		},
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
 	},
 }
