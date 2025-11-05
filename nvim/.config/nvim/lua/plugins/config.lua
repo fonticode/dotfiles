43697272d9f31a7b3a47eba3ec8d452a276893fb
fonti <ivan.fontana@xfarm.ag>
2025-11-05 16:59:32 +0100

    add nvim pomodoro timer

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -82,6 +82,70 @@ return {
 		},
 	},
 
+	--- pomo.nvim
+	--- usage:
+	--- :TimerStart 25m
+	--- :TimerSession [SESSION_NAME] # For configured session
+	{
+		"epwalsh/pomo.nvim",
+		version = "*", -- Recommended, use latest release instead of latest commit
+		lazy = true,
+		cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
+		dependencies = {
+			-- Optional, but highly recommended if you want to use the "Default" timer
+			"rcarriga/nvim-notify",
+		},
+		opts = {
+			-- You can optionally define custom timer sessions.
+			sessions = {
+				-- Example session configuration for a session called "pomodoro".
+				pomodoro = {
+					{ name = "Work", duration = "25m" },
+					{ name = "Short Break", duration = "5m" },
+					{ name = "Work", duration = "25m" },
+					{ name = "Short Break", duration = "5m" },
+					{ name = "Work", duration = "25m" },
+					{ name = "Short Break", duration = "5m" },
+					{ name = "Work", duration = "25m" },
+					{ name = "Long Break", duration = "15m" },
+				},
+			},
+		},
+	},
+
+	--- Obsidian
+	-- {
+	--
+	-- 	"epwalsh/obsidian.nvim",
+	-- 	version = "*", -- recommended, use latest release instead of latest commit
+	-- 	lazy = true,
+	-- 	ft = "markdown",
+	-- 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- 	-- event = {
+	-- 	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	-- 	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+	-- 	--   -- refer to `:h file-pattern` for more examples
+	-- 	--   "BufReadPre path/to/my-vault/*.md",
+	-- 	--   "BufNewFile path/to/my-vault/*.md",
+	-- 	-- },
+	-- 	dependencies = {
+	-- 		-- Required.
+	-- 		"nvim-lua/plenary.nvim",
+	--
+	-- 		-- see below for full list of optional dependencies 👇
+	-- 	},
+	-- 	opts = {
+	-- 		workspaces = {
+	-- 			{
+	-- 				name = "personal",
+	-- 				path = "~/Vault",
+	-- 			},
+	--
+	-- 			-- see below for full list of options 👇
+	-- 		},
+	-- 	},
+	-- },
+
 	--- Hardtime (learn proper vim motions)
 	-- {
 	-- 	"m4xshen/hardtime.nvim",
