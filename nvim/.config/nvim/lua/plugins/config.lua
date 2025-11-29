9f243f6f2e302ce232f1f46484194df3ecace438
fonti <ivan.fontana@xfarm.ag>
2025-11-29 11:01:44 +0100

    add nvim mermaid diagrams rendering

--- nvim/.config/nvim/lua/plugins/config.lua
+++ nvim/.config/nvim/lua/plugins/config.lua
@@ -112,6 +112,14 @@ return {
 			},
 		},
 	},
+	--- Mermaid diagrams rendering
+	{
+		"3rd/diagram.nvim",
+		dependencies = {
+			{ "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
+		},
+		opts = {},
+	},
 
 	--- Obsidian
 	-- {
